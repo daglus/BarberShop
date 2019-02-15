afc5affd4b5e86cfa0bb40306fd13acc965854bc
daglus <daglus.ck.ua@gmail.com>
2019-02-15 16:04:41 +0200

    Finish lesson #26

--- app.rb
+++ app.rb
@@ -3,26 +3,60 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end 
+	end
+
+end
+
+def get_db
+	db = SQLite3::Database.new 'barbershop.db'
+	db.results_as_hash = true
+	return db
+end
+
+before do
+	db = get_db
+	@barbers = db.execute 'select * from Barbers'
+end
+
 configure do
-  @db = SQLite3::Database.new 'barbershop.db'
-  @db.execute 'CREATE TABLE IF NOT EXISTS
-    "Users"
-    (
-	    "id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	    "username"	TEXT,
-	    "phone"	TEXT,
-	    "datetime"	TEXT,
-	    "barber"	TEXT,
-	    "color"	TEXT
-    )'
+	db = get_db
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Users"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"username" TEXT,
+			"phone" TEXT,
+			"datestamp" TEXT,
+			"barber" TEXT,
+			"color" TEXT
+		)'
+
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Barbers"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"name" TEXT
+		)'
+
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
-  erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
 
 get '/about' do
-  erb :about
+	erb :about
 end
 
 get '/visit' do
@@ -30,21 +64,45 @@ get '/visit' do
 end
 
 post '/visit' do
+
 	@username = params[:username]
 	@phone = params[:phone]
 	@datetime = params[:datetime]
 	@barber = params[:barber]
 	@color = params[:color]
 
-  hh = { :username => 'Введите имя',
-         :phone => 'Введите телефон',
-         :datetime => 'Введите дату визита' }
+	# хеш
+	hh = { 	:username => 'Введите имя',
+			:phone => 'Введите телефон',
+			:datetime => 'Введите дату и время' }
 
-  @error = hh.select {|key,_| params[key] == ""}.values.join(", ")
+	@error = hh.select {|key,_| params[key] == ""}.values.join(", ")
 
-  if @error != ''
-    return erb :visit
-  end
+	if @error != ''
+		return erb :visit
+	end
 
-	erb "OK, username is #{@username}, #{@phone}, #{@datetime}, #{@barber}, #{@color}"
+	db = get_db
+	db.execute 'insert into
+		Users
+		(
+			username,
+			phone,
+			datestamp,
+			barber,
+			color
+		)
+		values (?, ?, ?, ?, ?)', [@username, @phone, @datetime, @barber, @color]
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+
 end
+
+get '/showusers' do
+	db = get_db
+
+	@results = db.execute 'select * from Users order by id desc'
+
+	erb :showusers
+end
+
